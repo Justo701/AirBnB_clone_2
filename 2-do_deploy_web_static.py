364c9d3a2f2745479a4e2e49b550954f7c38d6fd
Korkou Justus <korkoujustus@gmail.com>
2023-07-22 01:36:16 -0700

    updated solutions

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,7 +8,7 @@ from os import path
 
 env.hosts = ['34.229.254.190', '54.162.76.20']
 env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
+env.key_filename = '~/.ssh/school'
 
 
 def do_deploy(archive_path):
